4576bd61d97d931a7a80919e3f3c1354413acf20
gyooks <yukinong03@gmail.com>
2025-08-08 00:04:59 +0800

    update

--- deploy.js
+++ deploy.js
@@ -1,6 +1,9 @@
 const ghpages = require("gh-pages");
 
-ghpages.publish("dist", function (err) {
+ghpages.publish("dist", {
+  repo: "https://github.com/gyooks/CSIT321_Website.git",
+  branch: "gh-pages"
+}, function (err) {
   if (err) {
     console.error("Deployment failed:\n", err);
   } else {
