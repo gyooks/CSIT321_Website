34f44e83bab7e9cf2dea183a94c72b92a0f84ca4
gyooks <yukinong03@gmail.com>
2025-08-15 03:13:18 +0800

    upload correct video file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -67,7 +67,7 @@ export default function Home() {
               controls
               playsInline
             >
-              <source src="/public/assets/promo video.mov" type="video/mp4" />
+              <source src="/assets/promoVideo.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           </motion.div>
